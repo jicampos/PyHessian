40725b2b96df1c247f75257daede2d42391d7568
Javier Campos <jcampos@fnal.gov>
2023-12-01 14:52:00 -0600

    Do not track unused graph tensors

--- pyhessian/utils.py
+++ pyhessian/utils.py
@@ -72,14 +72,14 @@ def get_params_grad(model, layers):
         layer_params[layer] = []
 
     for name, param in model.named_parameters():
-        if not param.requires_grad:
+        if not param.requires_grad or param.grad is None:
             continue
         params.append(param)
-        grads.append(torch.tensor(0.) if param.grad is None else param.grad + 0.)
+        grads.append(param.grad + 0.)
         for layer in layers:
             if layer in name:
                 layer_params[layer].append(param)
-                layer_grads[layer].append(torch.zeros(param.shape, requires_grad=True) if param.grad is None else param.grad + 0.)
+                layer_grads[layer].append(param.grad + 0.)
 
     return params, grads, layer_grads, layer_params
 
